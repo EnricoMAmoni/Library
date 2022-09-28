11ef25ac3e9bd6d29fb5a6d6f69cf0763b5f1bee
giorgia berretti <giorgiaberretti@MacBook-Pro-di-giorgia.local>
2022-09-28 15:24:43 +0200

    inserito bottoncino con drop down nella navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,43 +1,49 @@
 <nav class="navbar navbar-expand-lg">
-    <div class="container-fluid">
+  <div class="container-fluid">
       <a class="navbar-brand" href="{{route('welcome')}}"><i class="bi bi-book mx-2"></i></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item mx-2">
-            <a class="nav-link" aria-current="page" href="{{route('welcome')}}">Home</a>
-          </li>
-          <li class="nav-item mx-2">
-            <a class="nav-link" href="{{route('indexLibri')}}">Catalogo Libri</a>
-          </li>
-          {{-- <li class="nav-item mx-2"> --}}
-
-
-
-
-            @guest
-            <a class="nav-link" href="{{route('login')}}">Login</a>
-            
-            <a class="nav-link" href="{{route('register')}}">Registrati</a>
-            @else
-            <a class="nav-link" href="{{route('createLibri')}}">Inserisci Libri</a>
-            
-            <a class="nav-link" href="{{route('logout')}}" 
-                onclick="event.preventDefault(); 
-                document.getElementById('logout-form').submit();">Logout
+       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+       </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item mx-2">
+          <a class="nav-link" aria-current="page" href="{{route('welcome')}}">Home</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="{{route('indexLibri')}}">Catalogo Libri</a>
+        </li>
+        
+        @guest
+        <li>
+          <a class="nav-link" href="{{route('createLibri')}}">Inserisci Libri</a>
+        </li>
+        <li>
+          <div class="dropdown dropstart">
+             <a class="bi bi-person-circle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             </a>
-            <a class="nav-link userPageLink" href="{{route('userPage')}}">{{Auth::user()->name}}</a>
-            @endguest
-
-            <form method="POST" class="d-none" id="logout-form" action="{{ route('logout') }}">
+             <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="{{route('register')}}">Registrati</a></li>
+              <li><a class="dropdown-item" href="{{route('login')}}">Login</a></li>
+            </ul>
+          </div>
+         
+        </li>
+        @else
+        <li>
+          <a class="nav-link" href="{{route('logout')}}" 
+           onclick="event.preventDefault(); 
+           document.getElementById('logout-form').submit();">Logout
+          </a>
+          <form method="POST" class="d-none" id="logout-form" action="{{ route('logout') }}">
             @csrf
-            </form>
-          
-
-          {{-- </li> --}}
-        </ul>
-      </div>
-    </div>
-  </nav>
+          </form>
+        </li>
+        <li>
+          <a class="nav-link userPageLink" href="{{route('userPage')}}">Benvenuto {{Auth::user()->name}}</a>
+        </li>
+        @endguest
+    
+    </ul>
+  </div>
+</div>
+</nav>
